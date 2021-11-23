26a3f7b2da7fa3d8548abdbf401a39dd5fb98d9a
McCray <tubach75@hotmail.com>
2021-11-23 11:09:06 -0500

    set state equal to the bands data object

---
+++ jazz-bands-client/src/components/BandsState.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {bands} from '../data/bands'
+
+class BandsState extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            bands: bands
+        }
+    }
+
+    
+}
+
+export default BandsState
