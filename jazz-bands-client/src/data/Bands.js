5729149c1d2a444873fd6c32b26b5028ed8c4adc
McCray <tubach75@hotmail.com>
2021-11-23 12:51:57 -0500

    removed null values from bands data object

--- jazz-bands-client/src/data/Bands.js
+++ jazz-bands-client/src/data/Bands.js
@@ -1,8 +1,8 @@
 export const bands = [
     {band: "The Red Garland Trio",
-    personnel: [{name: "Red Garland", instrument: "Piano", bio: null, picture: null},
-                {name: "Art Taylor", instrument: "Drums", bio: null, picture: null},
-                {name: "Paul Chambers", instrument: "Bass", bio: null, picture: null}],
-    signatureSong: null
+    personnel: [{name: "Red Garland", instrument: "Piano", bio: "null", picture: "null"},
+                {name: "Art Taylor", instrument: "Drums", bio: "null", picture: "null"},
+                {name: "Paul Chambers", instrument: "Bass", bio: "null", picture: "null"}],
+    signatureSong: "null"
     }
 ]
