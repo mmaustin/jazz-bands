a8e85ec1080ef5ab0978471d5cd302ad1f8de55f
McCray <tubach75@hotmail.com>
2021-11-23 10:57:29 -0500

    added additional properties to the band object

--- jazz-bands-client/src/data/Bands.js
+++ jazz-bands-client/src/data/Bands.js
@@ -1,7 +1,8 @@
 export const bands = [
     {band: "The Red Garland Trio",
-    personnel: [{name: "Red Garland", instrument: "Piano"},
-                {name: "Art Taylor", instrument: "Drums"},
-                {name: "Paul Chambers", instrument: "Bass"}]
+    personnel: [{name: "Red Garland", instrument: "Piano", bio: null, picture: null},
+                {name: "Art Taylor", instrument: "Drums", bio: null, picture: null},
+                {name: "Paul Chambers", instrument: "Bass", bio: null, picture: null}],
+    signatureSong: null
     }
 ]
