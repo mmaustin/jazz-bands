014b1ea071e1e91ffa89d1edf01dad90347435ec
McCray <tubach75@hotmail.com>
2021-11-23 12:23:40 -0500

    preliminary build of band presentational component

--- jazz-bands-client/src/components/Band.js
+++ jazz-bands-client/src/components/Band.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Band = ({band}) => {
-
+    return(
+        <>
+            <li>
+                {band.band}
+            </li>
+            <p>
+                {band.personnel.map(p => p.name)}
+            </p>
+        </>
+    )
 }
 
 export default Band
