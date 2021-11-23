207a7f21ebf223a8444ba199861f5ea119baf982
McCray <tubach75@hotmail.com>
2021-11-23 10:43:46 -0500

    removed bands practice data from App component

--- jazz-bands-client/src/components/App.js
+++ jazz-bands-client/src/components/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 //import RouterComponent from './RouterComponent';
-import {bands} from '../data/Bands'
+
 
 class App extends Component {
     
 
   render() { 
-    let a = bands.map(b => b.personnel.map(m => {
-        return(
-            <>
-                <p>{m.name}</p>
-                <p>{m.instrument}</p>
-            </>
-        )
-    }))
 
     return (
       <div>
-        <p>{a}</p>
+        
       </div>
     );
   }
