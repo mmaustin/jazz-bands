1be112752b06cd54d604dc738900c1e97ee8c894
McCray <tubach75@hotmail.com>
2021-11-24 15:26:58 -0500

    added state names to handle submit function

--- jazz-bands-client/src/components/BandFile.js
+++ jazz-bands-client/src/components/BandFile.js
@@ -2,9 +2,34 @@ import React from 'react'
 //import Band from './Band'
 
 const BandFile = ({handleChange, state}) => {
+    /*this.handleSubmit = (event) => {
+        event.preventDefault();
+        let formData = {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+        };
+        let dataArray = this.state.submittedData.concat(formData);
+        this.setState({ submittedData: dataArray });
+      };*/
+
+    
+    this.handleSubmit =(e) => {
+        e.preventDefault();
+        let formData = {
+            bandName: state.bandName,
+            playerOne: {name: state.playerOne.name, instrument: state.playerOne.instrument},
+            playerTwo: {name: state.playerTwo.name, instrument: state.playerTwo.instrument},
+            playerThree: {name: state.playerThree.name, instrument: state.playerThree.instrument},
+            playerFour: {name: state.playerFour.name, instrument: state.playerFour.instrument},
+            playerFive: {name: state.playerFive.name, instrument: state.playerFive.instrument},
+            signatureSong: state.signatureSong
+        }
+    }
+
+    
     return(
         <div>
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
                 Band Name:
                 <input
                     type='text'
