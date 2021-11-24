29642acf2b37c9daa9ea4bc39cbfbfdcd1d759b5
McCray <tubach75@hotmail.com>
2021-11-24 14:15:07 -0500

    added first text field to test handle change function

--- jazz-bands-client/src/components/BandFile.js
+++ jazz-bands-client/src/components/BandFile.js
@@ -1,12 +1,38 @@
 import React from 'react'
 //import Band from './Band'
 
-const BandFile = () => {
+const BandFile = ({handleChange, state}) => {
     return(
-        <>
-            
-        </>
+        <div>
+            <form>
+                <input
+                    type='text'
+                    name='bandName'
+                    onChange={handleChange}
+                    value={state.bandName}
+                />
+            </form>
+        </div>
     )
 }
 
 export default BandFile
+
+/*
+      <div>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            onChange={(event) => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={(event) => this.props.handleChange(event)}
+            value={this.props.formData.lastName}
+          />
+        </form>
+      </div>
+*/
