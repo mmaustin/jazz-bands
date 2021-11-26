56dd9a4d6010fb62a16b28cbcc5aada5ea54d11d
McCray <tubach75@hotmail.com>
2021-11-26 07:43:30 -0500

    imported file form into state component

--- jazz-bands-client/src/components/BandsState.js
+++ jazz-bands-client/src/components/BandsState.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import BandFile from './BandFile';
+//import BandFile from './BandFile';
 //import Bands from './Bands';
 
 class BandsState extends Component {
@@ -28,9 +28,58 @@ class BandsState extends Component {
 
         return(
             <div>
-                <BandFile handleChange={this.handleChange} state={this.state}/>
-                {/*<p>{this.state.allBands.map((band, index) => <Bands key={index} band={band}/>)}</p>*/}
-                
+                <form onSubmit={this.handleSubmit}>
+                    Band Name:
+                    <input
+                        type='text'
+                        name='bandName'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.bandName}
+                    />
+                    Name:
+                    <input
+                        type='text'
+                        name='playerOne'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.playerOne}
+                    />
+                    Name:
+                    <input
+                        type='text'
+                        name='playerTwo'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.playerTwo}
+                    />
+                    Name:
+                    <input
+                        type='text'
+                        name='playerThree'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.playerThree}
+                    />
+                    Name:
+                    <input
+                        type='text'
+                        name='playerFour'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.playerFour}
+                    />
+                    Name:
+                    <input
+                        type='text'
+                        name='playerFive'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.playerFive}
+                    />
+                    Signature Song:
+                    <input
+                        type='text'
+                        name='signatureSong'
+                        onChange={this.props.handleChange}
+                        value={this.props.state.signatureSong}
+                    />
+                    <input type="submit"/>
+                </form>            
             </div>
         )
     }
