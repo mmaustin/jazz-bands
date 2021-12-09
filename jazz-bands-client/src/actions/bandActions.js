f8c4da62ddb90bf8fd6b356faf477c0d22109b52
McCray <tubach75@hotmail.com>
2021-12-09 10:18:31 -0500

    added async fetch bands action

---
+++ jazz-bands-client/src/actions/bandActions.js
@@ -0,0 +1,7 @@
+export const fetchBands = () => {
+    return (dispatch) => {
+      fetch("http://localhost:3000/bands")
+        .then((response) => response.json())
+        .then((bands) => dispatch({ type: "FETCH_BANDS", payload: bands }));
+    };
+  }
