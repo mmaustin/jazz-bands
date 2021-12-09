34f402a22043afbde30167b69a9436760511cfdd
McCray <tubach75@hotmail.com>
2021-12-09 10:26:57 -0500

    provided access to the redux store to app's components

--- jazz-bands-client/src/index.js
+++ jazz-bands-client/src/index.js
@@ -5,13 +5,18 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 //import { BrowserRouter as Router } from "react-router-dom";
 import thunk from "redux-thunk";
+import bandsReducer from './reducers/bandsReducer';
 import './index.css';
 import App from './components/App';
 
+const store = createStore(bandsReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <React.StrictMode>
+  //<React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
