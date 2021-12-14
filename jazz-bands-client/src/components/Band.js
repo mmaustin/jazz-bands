241cbc1c3ed7785e8ec5cc8d8f637ed483a9ea3c
McCray <tubach75@hotmail.com>
2021-12-14 13:32:57 -0500

    fixed issues with props and delete band dispatch

--- jazz-bands-client/src/components/Band.js
+++ jazz-bands-client/src/components/Band.js
@@ -2,17 +2,17 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { deleteBand } from '../actions/bandActions'
 
-const Band = ({band, color}) => {
+const Band = (props) => {
     return(
         <>
-            <p onClick={color}>{band.band_name}</p>
-            <p onClick={color}>{band.player_one}</p>
-            <p onClick={color}>{band.player_two}</p>
-            <p onClick={color}>{band.player_three}</p>
-            <p onClick={color}>{band.player_four}</p>
-            <p onClick={color}>{band.player_five}</p>
-            <p onClick={color}>{band.signature_song}</p>
-            <button onClick={() => this.props.deleteBand(band.id)}>Delete Band</button>
+            <p onClick={props.color}>{props.band.band_name}</p>
+            <p onClick={props.color}>{props.band.player_one}</p>
+            <p onClick={props.color}>{props.band.player_two}</p>
+            <p onClick={props.color}>{props.band.player_three}</p>
+            <p onClick={props.color}>{props.band.player_four}</p>
+            <p onClick={props.color}>{props.band.player_five}</p>
+            <p onClick={props.color}>{props.band.signature_song}</p>
+            <button onClick={() => props.deleteBand(props.band.id)}>Delete Band</button>
         </>
     )
 }
