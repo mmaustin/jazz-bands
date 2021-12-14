7a512d421d4993d1d7e5177ae0445613e7794319
McCray <tubach75@hotmail.com>
2021-12-14 13:10:52 -0500

    added delete_band case to reducer

--- jazz-bands-client/src/reducers/bandsReducer.js
+++ jazz-bands-client/src/reducers/bandsReducer.js
@@ -10,6 +10,11 @@ export default function bandsReducer(
             ...state,
             bands: [...state.bands, action.payload]
         };
+    case "DELETE_BAND":
+        return {
+            ...state,
+            bands: [...state.bands.filter(band => band.id !== action.payload)]
+        }
     case "CAP_BANDS":
         return {bands: state.bands.map(band => {return {...band, band_name: band.band_name
             .split( ' ').map(word => word.toLowerCase()).join(' ')}})}
