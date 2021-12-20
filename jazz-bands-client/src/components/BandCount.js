8894963404e4dbcd4722d9145968a7816fd0d32c
McCray <tubach75@hotmail.com>
2021-12-20 16:29:05 -0500

    added band count function to handle use state

---
+++ jazz-bands-client/src/components/BandCount.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const BandCount = () => {
+    const [count, setCount] = useState(0);
+
+    return(
+        <div>
+            <p></p>
+        </div>
+    )
+}
+
+export default BandCount
