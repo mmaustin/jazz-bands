d5cc940b740984e098aae5d9feae955ac901e9f9
McCray <tubach75@hotmail.com>
2021-12-20 16:34:53 -0500

    import band count component to its parent component

--- jazz-bands-client/src/components/BandCount.js
+++ jazz-bands-client/src/components/BandCount.js
@@ -5,7 +5,8 @@ const BandCount = () => {
 
     return(
         <div>
-            <p></p>
+            <p>How many times has the band's button been clicked: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Band's Button</button>
         </div>
     )
 }
