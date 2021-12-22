9c44fad8de6f928980f08fb3dd4d2d259c82368d
McCray <tubach75@hotmail.com>
2021-12-22 13:02:20 -0500

    attempting to display the document's url with effect hook

--- jazz-bands-client/src/components/BandCount.js
+++ jazz-bands-client/src/components/BandCount.js
@@ -3,14 +3,14 @@ import React, {useState, useEffect} from 'react'
 const BandCount = () => {
     const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        document.title = `You clicked ${count} times`;
+    let a = useEffect(() => {
+        return document.URL
     });
 
     return(
         <div>
             <p>How many times has the band's button been clicked: {count}</p>
-            {/*<p>{document.title}</p>*/}
+            <p>What the what?</p>
             <button onClick={() => setCount(count + 1)}>Band's Button</button>
         </div>
     )
