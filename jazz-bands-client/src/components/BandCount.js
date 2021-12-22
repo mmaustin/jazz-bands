23bd46eb0fc1c948688486b02f217e26e86f69b6
McCray <tubach75@hotmail.com>
2021-12-22 13:12:00 -0500

    I was using effect hook improperly, reverted to old code

--- jazz-bands-client/src/components/BandCount.js
+++ jazz-bands-client/src/components/BandCount.js
@@ -3,14 +3,13 @@ import React, {useState, useEffect} from 'react'
 const BandCount = () => {
     const [count, setCount] = useState(0);
 
-    let a = useEffect(() => {
-        return document.URL
+    useEffect(() => {
+        document.title = `This Is IT! ${count}`
     });
 
     return(
         <div>
             <p>How many times has the band's button been clicked: {count}</p>
-            <p>What the what?</p>
             <button onClick={() => setCount(count + 1)}>Band's Button</button>
         </div>
     )
