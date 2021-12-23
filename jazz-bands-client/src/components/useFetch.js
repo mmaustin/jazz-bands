b776cb2ea9aa6a7424e68d92c1025450eb6fe01a
McCray <tubach75@hotmail.com>
2021-12-23 16:30:00 -0500

    used debugger and console to inspect data and hook

--- jazz-bands-client/src/components/useFetch.js
+++ jazz-bands-client/src/components/useFetch.js
@@ -7,9 +7,10 @@ const useFetch = (url) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setData(data));
-  }, [url]);
+  });
 
   return [data];
+  
 };
 
 export default useFetch;
